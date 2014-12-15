6308acdaa9bc045903e27c2f0fad00578f22cd4e
jace.patel@gmail.com <jace.patel@gmail.com>
2014-12-15 21:03:31 +1000

    fuck me

--- web.js
+++ web.js
@@ -5,4 +5,7 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/app"));
+
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
 app.listen(process.env.PORT || 5000);
